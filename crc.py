9e8e58f6779eca3172955157a4d512a5b2684765
Nicola Coretti <nico.coretti@gmail.com>
2016-03-21 17:32:57 +0100

    ---/NiCo: intial commit

---
+++ crc.py
@@ -0,0 +1,309 @@
+import abc
+import enum
+import numbers
+
+
+class Byte(numbers.Number):
+
+    def __init__(self, value=0x00):
+        self._bitmask = 0xFF
+        self._bit_length = 8
+        self._value = value & self._bitmask
+
+    def __add__(self, other):
+        if not isinstance(other, Byte):
+            other = Byte(other)
+        return Byte(self.value + other.value)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __iadd__(self, other):
+        result = self + other
+        self.value = result.value
+        return self
+
+    def __eq__(self, other):
+        if not isinstance(other, Byte):
+            raise TypeError('unsupported operand')
+        return self.value == other.value
+
+    def __hash__(self):
+        return hash(self.value)
+
+    def __len__(self):
+        return self._bit_length
+
+    def __getitem__(self, item):
+        if item >= self._bit_length or item < 0:
+            raise IndexError
+        return (self.value & (1 << item)) >> item
+
+    def __int__(self):
+        return self.value
+
+    @property
+    def value(self):
+        return self._value & self._bitmask
+
+    @value.setter
+    def value(self, value):
+        self._value = value & self._bitmask
+
+    def reversed(self):
+        value = 0
+        index = 0
+        for bit in reversed(self):
+            value += self[index] << index
+            index += 1
+        return Byte(value)
+
+
+class CrcConfiguration(object):
+    """
+    A CrcConfiguration provides all settings necessary to determine the concrete
+    implementation of a specific crc algorithm/register.
+    """
+
+    def __init__(self, width, polynom, init_value=0, final_xor_value=0, reverse_input=False, reverse_output=False):
+        self._width = width
+        self._polynom = polynom
+        self._init_value = init_value
+        self._final_xor_value = final_xor_value
+        self._reverse_input = reverse_input
+        self._reverse_output = reverse_output
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def polynom(self):
+        return self._polynom
+
+    @property
+    def init_value(self):
+        return self._init_value
+
+    @property
+    def final_xor_value(self):
+        return self._final_xor_value
+
+    @property
+    def reverse_input(self):
+        return self._reverse_input
+
+    @property
+    def reverse_output(self):
+        return self._reverse_output
+
+
+@enum.unique
+class Crc8(enum.Enum):
+
+    CCITT = CrcConfiguration(8, 0x07, 0, 0, False, False)
+    SAEJ1850 = CrcConfiguration(8, 0x1D, 0, 0, False, False)
+
+
+@enum.unique
+class Crc16(enum.Enum):
+    pass
+
+
+@enum.unique
+class Crc32(enum.Enum):
+    pass
+
+
+def is_crc_configruation(configuration):
+    """
+    Checks whether or not a specified configuration is a crc configuration or not.
+
+    :param configuration: which will be checked.
+
+    :return: True if it is a valid crc configuration, otherwise False.
+    """
+    return isinstance(configuration, CrcConfiguration) \
+           or isinstance(configuration, Crc8) \
+           or isinstance(configuration, Crc16) \
+           or isinstance(configuration, Crc32)
+
+
+class AbstractCrcRegister(metaclass=abc.ABCMeta):
+    """
+    Abstract base class / Interface a crc register needs to implement.
+
+    Workflow:
+        1. The Crc-Register needs to be initialized.    1 time     (init)
+        2. Data is feed into the crc register.          1..n times (update)
+        3. Final result is calculated.                  1 time     (digest)
+    """
+
+    @abc.abstractmethod
+    def init(self):
+        """
+        Initializes the crc register.
+        """
+        pass
+
+    @abc.abstractmethod
+    def update(self, data):
+        """
+        Feeds the provided data into the crc register.
+
+        :param bytes data: a bytes like object or ann object which can be converted to a bytes
+                     like object using the built in bytes() function.
+        :return: the current value of the crc register.
+        """
+        pass
+
+    @abc.abstractmethod
+    def digest(self):
+        """
+        Final crc checksum will be calculated.
+
+        :return: the final crc checksum.
+        :rtype: int.
+        """
+        pass
+
+    @abc.abstractmethod
+    def reverse(self):
+        """
+        Calculates the reversed value of the crc register.
+
+        :return: the the reversed value of the crc register.
+        """
+        pass
+
+
+class CrcRegisterBase(AbstractCrcRegister):
+    """
+    Already implements commonly used methods, but still is an abstract class.
+
+    Subclasses need to implement the update method an comply to the following contract:
+    - tbd ...
+    """
+
+    def __init__(self, configuration):
+        if not is_crc_configruation(configuration):
+            raise Exception("No valid crc configuration provided")
+        if isinstance(configuration, enum.Enum):
+            configuration = configuration.value
+        self._topbit = 1 << (configuration.width - 1)
+        self._bitmask = 2 ** configuration.width - 1
+        self._config = configuration
+        self._register = configuration.init_value & self._bitmask
+
+    def __len__(self):
+        return self._config.width
+
+    def __getitem__(self, item):
+        if item >= self._config.width or item < 0:
+            raise IndexError
+        return (self.register & (1 << item)) >> item
+
+    def init(self):
+        self.register = self._config.init_value
+
+    @abc.abstractmethod
+    def update(self, data):
+        pass
+
+    def digest(self):
+        if self._config.reverse_output:
+            self.register = self.reverse()
+        return self.register ^ self._config.final_xor_value
+
+    def reverse(self):
+        index = 0
+        reversed_value = 0
+        for bit in reversed(self):
+            reversed_value += self[index] << index
+            index += 1
+        return reversed_value
+
+    def _is_division_possible(self):
+        return (self.register & self._topbit) > 0
+
+    @property
+    def register(self):
+        return self._register & self._bitmask
+
+    @register.setter
+    def register(self, value):
+        self._register = value & self._bitmask
+
+
+class CrcRegister(CrcRegisterBase):
+    """
+    A primitive loop based crc register.
+
+    .. note:
+
+        If performance is an important issue for the crc calcualation use table
+        based register.
+    """
+
+    def __init__(self, configuration):
+        super().__init__(configuration)
+
+    def update(self, data):
+        for byte in data:
+            byte = Byte(byte)
+            if self._config.reverse_input:
+                byte = byte.reversed()
+            self.register ^= int(byte) << (self._config.width - 8)
+            for bit in byte:
+                if self._is_division_possible():
+                    self.register = (self.register << 1) ^ self._config.polynom
+                else:
+                    self.register <<= 1
+        return self.register
+
+
+class TableBasedCrcRegister(CrcRegisterBase):
+
+    def __init__(self, configuration):
+        super().__init__(configuration)
+        self._lookup_table = None
+
+    def update(self, data):
+        raise NotImplementedError
+
+
+class CrcCalculator(object):
+
+    def __init__(self, configuration, table_driven_calculation=False):
+        self._crc_register = CrcRegister(configuration)
+
+    def calculate_checksum(self, data):
+        self._crc_register.init()
+        self._crc_register.update(data)
+        return self._crc_register.digest()
+
+
+def crc8(data, init_value=0x00, polynom=0x07, final_xor=0x00, reversed_input=False, reversed_output=False):
+    config = CrcConfiguration(8, polynom, init_value, final_xor, reversed_input, reversed_output)
+    crc_register = CrcRegister(config)
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+    return 244 #crc_register.calculate_checksum(data)
+
+from array import array
+LOOKUP_TABLES = {
+    Crc8: {
+       Crc8.CCITT: array('B',
+                         []
+                         ),
+       Crc8.SAEJ1850: array('B',
+                            []
+                           )
+    },
+    "Crc16": {
+
+    },
+    "Crc32": {
+
+    }
+}
---
+++ crc_tests.py
@@ -0,0 +1,145 @@
+import enum
+import string
+import unittest
+from crc import Byte, CrcRegister, crc8, is_crc_configruation
+from crc import CrcConfiguration, Crc8, Crc16, Crc32, CrcCalculator
+from collections import namedtuple
+
+
+class ByteTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_conversion_to_int(self):
+        int_value = 0x01
+        byte = Byte(int_value)
+        self.assertIsInstance(int(byte), int)
+        self.assertEqual(int(byte), int_value)
+
+    def test_is_equal_operator(self):
+        for value in range(0, 256):
+            self.assertEqual(Byte(value) == Byte(value), True)
+            self.assertEqual(Byte(value) == Byte(value + 1), False)
+
+    def test_adding_two_bytes_without_overflow(self):
+        lhs = Byte(0x02)
+        rhs = Byte(0x10)
+        expected_result = Byte(0x12)
+        self.assertEqual(lhs + rhs, expected_result)
+
+    def test_adding_an_integer_to_a_byte_without_overflow(self):
+        lhs = 0xF0
+        rhs = Byte(0x02)
+        expected_result = Byte(lhs + int(rhs))
+        self.assertEqual(lhs + rhs, expected_result)
+
+    def test_adding_two_bytes_with_overflow(self):
+        lhs = Byte(0xFA)
+        rhs = Byte(0xFA)
+        expected_result = Byte(0xFA + 0xFA)
+
+    def test_adding_an_integer_to_a_byte_with_overflow(self):
+        lhs = 0xFA
+        rhs = Byte(0xFA)
+        expected_result = Byte(lhs + int(rhs))
+        self.assertEqual(lhs + rhs, expected_result)
+        self.assertEqual(lhs + rhs, expected_result)
+
+    def test_instance_based_add(self):
+        lhs = Byte(0x00)
+        rhs = Byte(0xFA)
+        lhs += rhs
+        self.assertEqual(lhs._value, rhs._value)
+
+
+class CrcConfigurationTest(unittest.TestCase):
+
+    def setUp(self):
+        self.width = 8
+        self.polynom = 0x07
+        self.init_value = 0xFF
+        self.final_xor_value = 0xAA
+        self.reverse_input = True
+        self.reverse_output = True
+
+    def test_init_using_defaults(self):
+        configuration = CrcConfiguration(self.width, self.polynom)
+        self.assertEqual(configuration.width, self.width)
+        self.assertEqual(configuration.polynom, self.polynom)
+        self.assertEqual(configuration.init_value, 0)
+        self.assertEqual(configuration.final_xor_value, 0)
+        self.assertEqual(configuration.reverse_input, False)
+        self.assertEqual(configuration.reverse_output, False)
+
+    def test_init(self):
+        configuration = CrcConfiguration(self.width,
+                                         self.polynom,
+                                         self.init_value,
+                                         self.final_xor_value,
+                                         self.reverse_input,
+                                         self.reverse_output)
+
+        self.assertEqual(configuration.width, self.width)
+        self.assertEqual(configuration.polynom, self.polynom)
+        self.assertEqual(configuration.init_value, self.init_value)
+        self.assertEqual(configuration.final_xor_value, self.final_xor_value)
+        self.assertEqual(configuration.reverse_input, self.reverse_input)
+        self.assertEqual(configuration.reverse_output, self.reverse_output)
+
+
+class CrcModuleTest(unittest.TestCase):
+
+    def test_if_is_crc_configuration_returns_true_for_crc_configuration(self):
+        configurations = [CrcConfiguration(0,0), Crc8.CCITT, Crc8.SAEJ1850]
+        for configuration in configurations:
+            self.assertTrue(is_crc_configruation(configuration))
+
+    def test_if_is_crc_configuration_returns_false_for_none_crc_configuration(self):
+        configurations = [None, object(), "stuff", {}]
+        for configuration in configurations:
+            self.assertFalse(is_crc_configruation(configuration))
+
+
+class CrcRegisterTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init_works_with_enum(self):
+        config = Crc8.CCITT
+        crc_register = CrcRegister(config)
+
+
+
+class Crc8Test(unittest.TestCase):
+
+    def setUp(self):
+        CrcTestData = namedtuple('CrcTestData', 'data checksum')
+        self.crc8_test_suit = [
+            CrcTestData(data='', checksum=0x00),
+            CrcTestData(data=string.digits[1:], checksum=0xF4),
+            CrcTestData(data=string.digits[1:][::-1], checksum=0x91),
+            CrcTestData(data=string.digits, checksum=0x45),
+            CrcTestData(data=string.digits[::-1], checksum=0x6E),
+        ]
+
+    def test_crc8_smoke_test(self):
+        data = string.digits[1:]
+        expected = 0xF4
+        checksum = crc8(data)
+        self.assertEqual(checksum, expected)
+
+    def test_crc8_test_suit(self):
+        calculator = CrcCalculator(Crc8.CCITT)
+        for test in self.crc8_test_suit:
+            self.assertEqual(calculator.calculate_checksum(test.data.encode('utf-8')), test.checksum)
+
+class Crc16Test(unittest.TestCase):
+    pass
+
+class Crc32Test(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()

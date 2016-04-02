3a67f78eaf7e34edde72b167fa77a6ee87704863
Nicola Coretti <nico.coretti@gmail.com>
2016-04-02 16:11:06 +0200

    ---/NiCo: refactored table generator tool.

--- crc/tools/table_generator.py
+++ crc/tools/table_generator.py
@@ -1,21 +1,5 @@
-from crc import crc
 import docopt
-
-
-def create_lookup_table(width, polynom):
-    config = crc.CrcConfiguration(width=width, polynom=polynom)
-    crc_register = crc.CrcRegister(config)
-    lookup_table = list()
-    for index in range(0, 256):
-        crc_register.init()
-        data = bytes((index).to_bytes(1, byteorder='big'))
-        crc_register.update(data)
-        lookup_table.append(crc_register.digest())
-    return lookup_table
-
-
-def print_lookup_table(lookup_table):
-    print(lookup_table)
+from crc.crc import create_lookup_table
 
 
 def main():
@@ -27,7 +11,7 @@ def main():
     width = int(args['<width>'])
     polynom = int(args['<polynom>'], 16)
     lookup_table = create_lookup_table(width, polynom)
-    print_lookup_table(lookup_table)
+    print("{}".format(lookup_table))
 
 
 if __name__ == '__main__':

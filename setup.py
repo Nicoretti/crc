731743851ac3ca396989cf9d7f7c6a7582df63c7
Nicola Coretti <nico.coretti@gmail.com>
2021-03-17 21:39:39 +0100

    Update package description and bump version number

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
-from setuptools import setup, find_packages
+import pathlib
 from crc.crc import LIBRARY_VERSION
+from setuptools import setup, find_packages
 
-setup(
-    name='crc',
-    version=LIBRARY_VERSION,
-    packages=find_packages(),
-    install_requires=['docopt>=0.6.2'],
-    url='https://github.com/Nicoretti/crc',
-    license='BSD',
-    author='Nicola Coretti',
-    author_email='nico.coretti@gmail.com',
-    description='TBD',
-    keywords=['CRC', 'CRC8', 'CRC16', 'CRC32'],
-    entry_points={
-        'console_scripts': [
-            'crc=crc.tools:main',
-        ],
-        'crc.cli.command': ['table=crc.tools:table']
-    }
-)
+current = pathlib.Path(__file__).parent.resolve()
+long_description = (current / 'README.md').read_text(encoding='utf-8')
+
+if __name__ == '__main__':
+    setup(
+        name='crc',
+        version=LIBRARY_VERSION,
+        packages=find_packages(),
+        install_requires=['docopt>=0.6.2'],
+        url='https://github.com/Nicoretti/crc',
+        license='BSD',
+        author='Nicola Coretti',
+        author_email='nico.coretti@gmail.com',
+        description='Library and CLI tool for calculating and verifying CRC checksums.',
+        keywords=['CRC', 'CRC8', 'CRC16', 'CRC32', 'CRC64'],
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        entry_points={
+            'console_scripts': [
+                'crc=crc.tools:main',
+            ],
+            'crc.cli.command': ['table=crc.tools:table']
+        }
+    )

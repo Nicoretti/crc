8012bf9a989823084df99cfb5e3d947873841516
Nicola Coretti <nico.coretti@gmail.com>
2021-03-17 22:05:43 +0100

    Update setup.py file

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 import pathlib
+import unittest
 from crc.crc import LIBRARY_VERSION
 from setuptools import setup, find_packages
 
 current = pathlib.Path(__file__).parent.resolve()
-long_description = (current / 'README.md').read_text(encoding='utf-8')
+
+
+def tests():
+    return unittest.defaultTestLoader.discover(start_dir=f'{current.resolve()}', pattern='*tests.py')
+
+
+def readme():
+    return (current / 'README.md').read_text(encoding='utf-8')
+
 
 if __name__ == '__main__':
     setup(
         name='crc',
         version=LIBRARY_VERSION,
+        test_suite='setup.tests',
         packages=find_packages(),
+        classifiers=[
+            'Programming Language :: Python :: 3.6',
+        ],
         install_requires=['docopt>=0.6.2'],
         url='https://github.com/Nicoretti/crc',
         license='BSD',
@@ -17,7 +30,7 @@ if __name__ == '__main__':
         author_email='nico.coretti@gmail.com',
         description='Library and CLI tool for calculating and verifying CRC checksums.',
         keywords=['CRC', 'CRC8', 'CRC16', 'CRC32', 'CRC64'],
-        long_description=long_description,
+        long_description=readme(),
         long_description_content_type='text/markdown',
         entry_points={
             'console_scripts': [
